8894d7b0e610d5d20b19b5dac696d0d1db526dd4
Jorge Sánchez <xurxosf@gmail.com>
2019-08-19 13:28:08 +0200

    Add title to gradient toolbar

--- lib/gradient_back.dart
+++ lib/gradient_back.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class GradientBack extends StatelessWidget {
+  final String title;
+
+  GradientBack(this.title);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -18,6 +22,17 @@ class GradientBack extends StatelessWidget {
           tileMode: TileMode.clamp,
         ),
       ),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+          fontFamily: "Lato",
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      alignment: Alignment(-0.9, -0.6),
     );
   }
 }
