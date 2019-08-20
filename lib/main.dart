982444507ac052d3f05b6a33a081b766b22564e8
Jorge Sánchez <xurxosf@gmail.com>
2019-08-20 17:59:11 +0200

    Create bottom navigation bar and profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'description_place.dart';
-import 'header_appbar.dart';
-import 'review_list.dart';
-import 'gradient_back.dart';
+import 'platzi_trips_cupertino.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String descriptionDummy =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -27,19 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 4, descriptionDummy),
-                ReviewList()
-              ],
-            ),
-            HeaderAppBar()
-          ],
-        ),
-      ),
+      home: PlatziTripsCupertino(),
     );
   }
 }
